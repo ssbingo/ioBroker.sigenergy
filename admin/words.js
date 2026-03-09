319ab8164fb1d1b25e9d3507c60e7979c1a7944a
ssbingo <s.sternitzke@online.de>
2026-03-09 21:20:27 +0100

    .

--- admin/words.js
+++ admin/words.js
@@ -1,23 +1,4 @@
 /*global systemDictionary:true */
 'use strict';
 
-systemDictionary = {
-    "Sigenergy adapter settings": {
-        "en": "Sigenergy adapter settings",
-        "de": "Sigenergy Adapter-Einstellungen",
-        "ru": "Настройки адаптера Sigenergy",
-        "pt": "Configurações do adaptador Sigenergy",
-        "nl": "Sigenergy adapterinstellingen",
-        "fr": "Paramètres de l'adaptateur Sigenergy",
-        "it": "Impostazioni dell'adattatore Sigenergy",
-        "es": "Ajustes del adaptador Sigenergy",
-        "pl": "Ustawienia adaptera Sigenergy",
-        "uk": "Налаштування адаптера Sigenergy",
-        "zh-cn": "Sigenergy 适配器设置"
-    },
-    "IP Address": {
-        "en": "IP Address",
-        "de": "IP-Adresse",
-        "ru": "IP-адрес"
-    }
-};
+systemDictionary = {};
