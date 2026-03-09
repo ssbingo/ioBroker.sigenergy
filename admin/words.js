02ae230700b8695ba778db2156a64a7889f7e6ea
ssbingo <s.sternitzke@online.de>
2026-03-09 20:48:26 +0100

    .

--- admin/words.js
+++ admin/words.js
@@ -1,4 +1,18 @@
 /*global systemDictionary:true */
 'use strict';
 
-systemDictionary = {};
+systemDictionary = {
+    "sigenergy adapter settings": {
+        "en": "Sigenergy adapter settings",
+        "de": "Sigenergy Adapter-Einstellungen",
+        "ru": "????????? ???????? Sigenergy",
+        "pt": "Configurações do adaptador Sigenergy",
+        "nl": "Sigenergy adapterinstellingen",
+        "fr": "Paramètres de l'adaptateur Sigenergy",
+        "it": "Impostazioni dell'adattatore Sigenergy",
+        "es": "Ajustes del adaptador Sigenergy",
+        "pl": "Ustawienia adaptera Sigenergy",
+        "uk": "???????????? ???????? Sigenergy",
+        "zh-cn": "Sigenergy ?????"
+    }
+};
